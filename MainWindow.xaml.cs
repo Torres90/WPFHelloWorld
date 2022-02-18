f0f0a46403ecae0c391ddaf733095f7e2900eeb7
Torres Francisco <francisco.tadeu.torres@gmail.com>
2022-02-18 14:07:56 +0000

    Trying stack panels, buttons, labels, etc..

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,28 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFHelloWorld
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+
+    //MainWindow.xaml.cs is a partial class because it is combined with the XAML file
     public partial class MainWindow : Window
     {
+        int timesClicked = 0;
         public MainWindow()
         {
+            //methods runs xaml code
             InitializeComponent();
+            //myButton.FontSize = 30;
+            //myButton.Content = "Press lots to find out how much I love you";
+
+            //TextBlock textBlock = new();
+            //textBlock.Text = "Hello World";
+            //this.Content = textBlock;
+
         }
+
+
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            OpenURL("https://www.google.com/");
+        }
+
+        private static void OpenURL(string url) { Process.Start(new ProcessStartInfo { FileName = url, UseShellExecute = true }); }
+
+        //private void MyButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //myButton.FontSize += 10;
+        //    timesClicked++;
+
+        //    StringBuilder sb = new StringBuilder("I love you th");
+        //    for (int i = 0; i < timesClicked; i++)
+        //    {
+        //        sb.Append("i");
+        //    }
+        //    sb.Append("s much!");
+
+        //    myButton.Content = sb.ToString();
+        //}
     }
 }
